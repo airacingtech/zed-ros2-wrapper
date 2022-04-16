26a10c32b97f6a69daac6a9ff6bce887c5397c55
sisaha9 <sisahawork@gmail.com>
2022-04-15 20:11:48 -0700

    reverting changes

--- zed_wrapper/launch/include/zed_camera.launch.py
+++ zed_wrapper/launch/include/zed_camera.launch.py
@@ -6,7 +6,7 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, Command
 from launch_ros.actions import Node
 
 
@@ -24,6 +24,12 @@ def generate_launch_description():
     config_camera_path = LaunchConfiguration('config_camera_path')
 
     base_frame = LaunchConfiguration('base_frame')
+    cam_pos_x = LaunchConfiguration('cam_pos_x')
+    cam_pos_y = LaunchConfiguration('cam_pos_y')
+    cam_pos_z = LaunchConfiguration('cam_pos_z')
+    cam_roll = LaunchConfiguration('cam_roll')
+    cam_pitch = LaunchConfiguration('cam_pitch')
+    cam_yaw = LaunchConfiguration('cam_yaw')
 
     # ZED Configurations to be loaded by ZED Node
     default_config_common = os.path.join(
@@ -31,6 +37,15 @@ def generate_launch_description():
         'config',
         'common.yaml'
     )
+    
+    xacro_path = LaunchConfiguration('xacro_path')
+    
+    default_xacro_path = os.path.join(
+        get_package_share_directory('zed_wrapper'),
+        'urdf',
+        'zed_descr.urdf.xacro'
+    )
+
 
     # Declare the launch arguments
     declare_camera_name_cmd = DeclareLaunchArgument(
@@ -55,6 +70,11 @@ def generate_launch_description():
         'config_camera_path',
         description='Path to the `<camera_model>.yaml` file.')
 
+    declare_xacro_path_cmd = DeclareLaunchArgument(
+        'xacro_path',
+        default_value=default_xacro_path,
+        description='Path to the camera URDF file as a xacro file.')
+
     declare_svo_path_cmd = DeclareLaunchArgument(
         'svo_path',
         default_value='live', # 'live' used as patch for launch files not allowing empty strings as default parameters
@@ -64,6 +84,60 @@ def generate_launch_description():
         'base_frame',
         default_value='base_link',
         description='Name of the base link.')
+    
+    declare_pos_x_cmd = DeclareLaunchArgument(
+        'cam_pos_x',
+        default_value='0.0',
+        description='Position X of the camera with respect to the base frame.')
+
+    declare_pos_y_cmd = DeclareLaunchArgument(
+        'cam_pos_y',
+        default_value='0.0',
+        description='Position Y of the camera with respect to the base frame.')
+
+    declare_pos_z_cmd = DeclareLaunchArgument(
+        'cam_pos_z',
+        default_value='0.0',
+        description='Position Z of the camera with respect to the base frame.')
+
+    declare_roll_cmd = DeclareLaunchArgument(
+        'cam_roll',
+        default_value='0.0',
+        description='Roll orientation of the camera with respect to the base frame.')
+
+    declare_pitch_cmd = DeclareLaunchArgument(
+        'cam_pitch',
+        default_value='0.0',
+        description='Pitch orientation of the camera with respect to the base frame.')
+
+    declare_yaw_cmd = DeclareLaunchArgument(
+        'cam_yaw',
+        default_value='0.0',
+        description='Yaw orientation of the camera with respect to the base frame.')
+    
+    # Robot State Publisher node
+    rsp_node = Node(
+        package='robot_state_publisher',
+        namespace=camera_name,
+        executable='robot_state_publisher',
+        name='zed_state_publisher',
+        output='screen',
+        parameters=[{
+            'robot_description': Command(
+                [
+                    'xacro', ' ', xacro_path, ' ',
+                    'camera_name:=', camera_name, ' ',
+                    'camera_model:=', camera_model, ' ',
+                    'base_frame:=', base_frame, ' ',
+                    'cam_pos_x:=', cam_pos_x, ' ',
+                    'cam_pos_y:=', cam_pos_y, ' ',
+                    'cam_pos_z:=', cam_pos_z, ' ',
+                    'cam_roll:=', cam_roll, ' ',
+                    'cam_pitch:=', cam_pitch, ' ',
+                    'cam_yaw:=', cam_yaw
+                ])
+        }]
+    )
 
     # Set LOG format
     os.environ['RCUTILS_CONSOLE_OUTPUT_FORMAT'] = '{time} [{name}] [{severity}] {message}'
@@ -96,8 +170,16 @@ def generate_launch_description():
     ld.add_action(declare_node_name_cmd)
     ld.add_action(declare_config_common_path_cmd)
     ld.add_action(declare_config_camera_path_cmd)
+    ld.add_action(declare_xacro_path_cmd)
     ld.add_action(declare_svo_path_cmd)
     ld.add_action(declare_base_frame_cmd)
+    ld.add_action(declare_pos_x_cmd)
+    ld.add_action(declare_pos_y_cmd)
+    ld.add_action(declare_pos_z_cmd)
+    ld.add_action(declare_roll_cmd)
+    ld.add_action(declare_pitch_cmd)
+    ld.add_action(declare_yaw_cmd)
+    ld.add_action(rsp_node)
     ld.add_action(zed_wrapper_node)
 
     return ld
